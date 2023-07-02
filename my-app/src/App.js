2695e998e26bc06b6487b0ce81b0b4b56731404e
Shabucky1812 <shaunbuck1812@gmail.com>
2023-07-02 16:41:19 +0000

    Render list items using map and nested components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,18 @@
 import css from './App.module.css';
-// import Sidebar from './components/Sidebar';
+import Sidebar from './components/Sidebar';
 // import NavBarSimple from './components/NavBarSimple';
 import NavBarForm from './components/NavBarForm';
+import Content from './components/Content';
 
 function App() {
-  return (
-    <div className={css.App}>
-      {/* <Sidebar /> */}
-      {/* <NavBarSimple /> */}
-      <NavBarForm />
-    </div>
-  );
+    return (
+        <div className={css.App}>
+            {/* <NavBarSimple /> */}
+            <NavBarForm />
+            <Sidebar />
+            <Content />
+        </div>
+    );
 }
 
 export default App;
