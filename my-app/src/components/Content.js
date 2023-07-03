9ed8f3472fc9c8743b1312146754205a66c01025
Shabucky1812 <shaunbuck1812@gmail.com>
2023-07-03 17:24:21 +0000

    Use mounting to create a loader

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import css from "./css/Content.module.css"
 import {savedPosts} from "../posts.json"
 import PostItem from './PostItem'
+import Loader from './Loader'
 
 export class Content extends Component {
 
@@ -9,8 +10,16 @@ export class Content extends Component {
         super(props)
 
         this.state = {
-
+            isLoaded: false
         }
+    }
+
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({
+                isLoaded: true
+            })
+        }, 2000)
     }
 
     render() {
@@ -20,7 +29,7 @@ export class Content extends Component {
                     <h1>My Photos</h1>
                 </div>
                 <div className={css.SearchResults}>
-                    <PostItem savedPosts={savedPosts}/>
+                    {this.state.isLoaded ? (<PostItem savedPosts={savedPosts}/>) : (<Loader />)}
                 </div>
             </div>
         )
