dd13faf249cd429ac1ddfff01909cedcfe285f6a
Shabucky1812 <shaunbuck1812@gmail.com>
2023-07-08 14:10:30 +0000

    API requests with hooks

---
+++ my-app/src/components/ContentAPIHooks.js
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from 'react'
+import css from "./css/Content.module.css"
+import PostItemAPI from './PostItemAPI'
+import Loader from './Loader'
+import axios from 'axios'
+import API_KEY from '../secrets'
+
+function ContentAPIHooks() {
+
+    const [isLoaded, setIsLoaded] = useState(false)
+    const [posts, setPosts] = useState([])
+    const [savedPosts, setSavedPosts] = useState([])
+
+    const fetchImages = async () => {
+        const response = await axios.get(`https://pixabay.com/api/?key=${API_KEY}&per_page=100`)
+        const fetchedPosts = response.data.hits
+        setIsLoaded(true)
+        setPosts(fetchedPosts)
+        setSavedPosts(fetchedPosts)
+    }
+
+    const handleSearch = (event) => {
+        let name = event.target.value.toLowerCase()
+        const filteredPosts = savedPosts.filter((post) => post.user.toLowerCase().includes(name))
+        setPosts(filteredPosts)
+    }
+
+    useEffect(() => {
+        fetchImages()
+    }, [])
+
+    return (
+        <div>
+            <div className={css.TitleBar}>
+                <h1>My Photos</h1>
+                <form>
+                    <label htmlFor="searchInput">Search:</label>
+                    <input type="text" placeholder="By Author" id="searchInput" onChange={handleSearch} />
+                    <h4>posts found: {posts.length}</h4>
+                </form>
+            </div>
+            <div className={css.SearchResults}>
+                {isLoaded ? (<PostItemAPI savedPosts={posts} />) : (<Loader />)}
+            </div>
+        </div>
+    )
+}
+
+export default ContentAPIHooks
